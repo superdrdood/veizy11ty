7749078f06357011f63a5a268a1374bb5f468d23
SuperDrDood <super.dr.dood@gmail.com>
2022-06-12 15:36:38 +0100

    Rum tracker!

---
+++ js/rumTracker/rumTracker.js
@@ -0,0 +1,343 @@
+let rumListGood;
+let rumValues;
+let rumTemplate;
+let rumTracker;
+let rumSorter;
+let rumListSort;
+let mixerList;
+let rumInfoTemplate;
+
+let newRumSort;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    rumStart(); // Sets everything up
+    rumCreate(); // Draws the rums to the page
+    rumSort("Name"); // Initial sorts the rum
+});
+
+document.addEventListener('click', function(event) {
+    if (event.target.dataset.sortvalue) {
+        rumSort(event.target.dataset.sortvalue);
+    }
+    if (event.target.dataset.mixer) {
+        toggleMixer(event.target.dataset.mixer);
+    }
+    if (event.target.closest(".rum")) {
+        if (event.target.closest(".rum").classList.contains("chosenRum")) {
+            clearRum();
+            clearSide();
+        } else if (document.querySelector("#rumTracker.chosenMixer")) {
+            if (event.target.closest(".rum.chosenMixer")) {
+                clearRum();
+                selectRum(event.target.closest(".rum").dataset.index);
+            }
+        } else {
+            clearRum();
+            selectRum(event.target.closest(".rum").dataset.index);
+        }
+    }
+    if (event.target.id == "rumClose") {
+        clearRum();
+    }
+});
+
+function rumStart() {
+    rumListGood = rumListExplode();
+    rumValues = Object.keys(rumListGood[0]);    
+    rumTemplate = document.querySelector("#rumTracker .rum").innerHTML;    
+    rumTracker = document.getElementById("rumBig");
+    rumSorter = document.getElementById("rumSorter");
+    rumSorter.innerHTML = "";
+
+    rumInfoTemplate = document.querySelector("#rumSingleText").innerHTML;
+
+    let rumSort = document.createElement("li");
+    rumSort.innerHTML = rumValues[0];
+    rumSort.dataset.sortvalue = rumValues[0];
+    rumSorter.appendChild(rumSort);
+
+    let rumSorters = ["Name","Percentage", "Price", "Price per Unit", "Height", "Style"];
+
+    for (let i = 1; i < rumSorters.length; i++) {
+        let rumSort = document.createElement("li");
+        rumSort.innerHTML = rumSorters[i];
+        rumSort.dataset.sortvalue = rumSorters[i];
+        rumSorter.appendChild(rumSort);
+    }
+
+    let mixerElement = document.querySelector("#rumMixerPicker");
+
+    for (let i = 0; i < mixerList.length; i++) {
+        let newMixerSelect = document.createElement("li");
+        newMixerSelect.innerHTML = mixerList[i];
+        newMixerSelect.dataset.mixer = mixerList[i];
+        mixerElement.appendChild(newMixerSelect);
+    }
+
+    clearAll();
+
+}
+
+function rumCreate() {
+
+    rumTracker.innerHTML = "";
+
+    for (let i = 0; i < rumListGood.length; i++) {
+
+        let rumGuy = document.createElement("div");
+        rumGuy.classList.add("rum");        
+        rumGuy.innerHTML = rumTemplate;
+        rumGuy.dataset.index = rumListGood[i]["index"];
+        let rumMixings = Object.keys(rumListGood[i]["mix"]);
+
+        rumGuy.dataset.mixers = "";
+
+        for (let ii = 0; ii < rumMixings.length; ii++) {
+            rumGuy.dataset.mixers += "[" + rumMixings[ii] + "]";
+        }
+
+        rumGuy.style.order = i;
+
+        if (UrlExists("rums/" +  rumListGood[i].Name.toLowerCase() + ".png")) {
+            rumGuy.querySelector(".rumImage img").src = "rums/" +  rumListGood[i].Name.toLowerCase() + ".png";
+        }
+
+        rumGuy.querySelector(".rumImage img").style.maxHeight =  ((rumListGood[i].Height / 325) * 200) + "px";
+
+        rumTracker.appendChild(rumGuy);        
+
+
+    }
+}
+
+function rumSort(sortType) {
+    sortElement = document.querySelector("[data-sortvalue='" + sortType + "'");
+    rev = false;
+    if (sortElement.classList.contains("rumValReverse")) {
+        rev = false;
+        sortElement.classList.remove("rumValReverse");
+    } else if (sortElement.classList.contains("rumValSelected")) {
+        rev = true;
+        sortElement.classList.add("rumValReverse");
+    } else {
+        if (document.querySelector(".rumValSelected")){
+            document.querySelector(".rumValSelected").classList.remove("rumValSelected");
+        }
+        if (document.querySelector(".rumValReverse")){
+            document.querySelector(".rumValReverse").classList.remove("rumValReverse");
+        }
+        document.querySelector("[data-sortvalue='" + sortType + "'").classList.add("rumValSelected");
+        rev = false;
+    }
+    rumListSort = JSON.parse(JSON.stringify(rumListGood));
+
+    rumListSort.sort((a, b) => a[sortType].localeCompare(b[sortType]));
+    if (rev) {
+        rumListSort.sort((b, a) => a[sortType].localeCompare(b[sortType]));
+    }
+
+    for (let i = 0; i < rumListSort.length; i++) {
+        document.querySelector("[data-index='" + rumListSort[i]["index"] + "']").style.order = i;
+        
+    }
+    if (document.querySelector(".sortedMixer")) {
+        document.querySelector(".sortedMixer").classList.remove("sortedMixer");
+    }
+
+}
+
+function rumListExplode() {
+    let newRumList = [];
+    properList = rumList2.split("\n");
+
+    titlesListTemp = properList[0].split("|MIX|");
+
+    titlesList = titlesListTemp[0].split("|");
+    mixerList = titlesListTemp[1].split("|");
+
+    for (let i = 1; i < properList.length; i++) {
+        let newRumElement = {};
+
+        let newRumMix = properList[i].split("|MIX|");
+
+        let newRumTemp = newRumMix[0].split("|");
+        let newMix = newRumMix[1].split("|");
+
+        
+        let newMixElement = {}
+
+        for (let ii = 0; ii < newMix.length; ii++) {
+            if (newMix[ii]) {
+                newMixElement[mixerList[ii]] = newMix[ii];
+            }
+        }   
+
+        for (let ii = 0; ii < newRumTemp.length; ii++) {
+            newRumElement[titlesList[ii]] = newRumTemp[ii];
+        }
+        
+        newRumElement["mix"] = newMixElement;
+        
+        newRumList.push(newRumElement);
+
+    }
+
+    for (let i = 0; i < newRumList.length; i++) {
+        newRumList[i]["index"] = i.toString();
+    }
+
+    return newRumList;
+
+}
+
+function selectRum(rum) { // chooses a rum when given the rum index
+    clearSide();
+    document.querySelector("#rumTracker").classList.add("chosenRum");
+
+    document.querySelector("[data-index='" + rum + "']").classList.add("chosenRum");
+    
+    chosenRum = rumListGood[rum];
+
+    newRumInfo = document.createElement("div");
+    newRumInfo.innerHTML = rumInfoTemplate;
+    
+    newRumInfo.querySelector("#rumSingleTitle").innerHTML = chosenRum["Name"];
+    newRumInfo.querySelector("#rumVolume").innerHTML = chosenRum["Volume"];
+    newRumInfo.querySelector("#rumPrice").innerHTML = chosenRum["Price"];
+    newRumInfo.querySelector("#rumPercent").innerHTML = chosenRum["Percentage"];
+    newRumInfo.querySelector("#rumUnits").innerHTML = chosenRum["Units"];
+    newRumInfo.querySelector("#rumUnitPrice").innerHTML = chosenRum["Price per Unit"];
+    newRumInfo.querySelector("#rumStatsImg img").src = "rums/" +  rumListGood[rum].Name.toLowerCase() + ".png";
+    newRumInfo.querySelector("#rumNotes").innerHTML = chosenRum["Notes"];
+    newRumTasty = newRumInfo.querySelector("#rumTasty")
+
+    document.querySelector("#rumSingleText").appendChild(newRumInfo);
+
+    thisRumKeys = Object.keys(chosenRum["mix"]);
+
+    thisRumImg = document.createElement("img");
+    thisRumImg.src = "rums/" +  rumListGood[rum].Name.toLowerCase() + ".png";
+
+    styleBox = document.querySelector("#rumStyleBox");
+
+
+    newImgScale = document.createElement("div");
+    newImgScale.classList.add("rumImgScale");
+    
+    for (let i = 0; i < chosenRum["Style"]; i++) {
+        cloneImg = thisRumImg.cloneNode()
+        newImgScale.appendChild(cloneImg);
+    }
+    for (let i = chosenRum["Style"]; i < 5; i++) {
+        cloneImg = thisRumImg.cloneNode();
+        cloneImg.classList.add("badRum");
+        newImgScale.appendChild(cloneImg);
+    }
+
+    styleBox.appendChild(newImgScale);
+
+    for (let i = 0; i < thisRumKeys.length; i++) {
+        newImgScale = document.createElement("div");
+        newImgScale.classList.add("rumImgScale");
+        
+        for (let ii = 0; ii < chosenRum["mix"][thisRumKeys[i]]; ii++) {
+            cloneImg = thisRumImg.cloneNode()
+            newImgScale.appendChild(cloneImg);
+        }
+        for (let ii = chosenRum["mix"][thisRumKeys[i]]; ii < 5; ii++) {
+            cloneImg = thisRumImg.cloneNode();
+            cloneImg.classList.add("badRum");
+            newImgScale.appendChild(cloneImg);
+        }
+
+        newImgScale.innerHTML += thisRumKeys[i];
+        newRumTasty.appendChild(newImgScale);
+    }
+}
+
+function clearRum() { // unselects the rum
+    if (document.querySelector(".rum.chosenRum")) {
+        document.querySelector(".rum.chosenRum").classList.remove("chosenRum");
+    }
+    if (document.querySelector("#rumTracker.chosenRum")) {
+        document.querySelector("#rumTracker.chosenRum").classList.remove("chosenRum");
+    }
+}
+
+function clearMixer() { // unselects any mixer
+    let mixers = document.querySelectorAll(".chosenMixer");
+    for (let i = 0; i < mixers.length; i++) {
+        mixers[i].classList.remove("chosenMixer");
+    }
+    if (document.querySelector(".sortedMixer")) {
+        document.querySelector(".sortedMixer").classList.remove("sortedMixer");
+    }    
+}
+
+function clearSide() { // clears the sidebar
+    document.querySelector("#rumSingleText").innerHTML = "";
+}
+
+function clearAll() {
+    clearRum();
+    clearMixer();
+    clearSide();
+}
+
+function toggleMixer(mixer) {
+    if (document.querySelector(".sortedMixer.chosenMixer[data-mixer*='" + mixer + "']")) {
+        clearMixer();
+        rumSort(document.querySelector(".rumValSelected").dataset.sortvalue);
+        rumSort(document.querySelector(".rumValSelected").dataset.sortvalue);
+    } else if (document.querySelector(".chosenMixer[data-mixer*='" + mixer + "']")) {
+        rumSortMix(mixer);
+        document.querySelector(".chosenMixer[data-mixer*='" + mixer + "']").classList.add("sortedMixer");
+    } else {
+        if (!document.querySelector(".rum.chosenRum[data-mixers*='" + mixer + "']")) {
+            clearAll();
+        }
+        clearMixer();
+        rumSort(document.querySelector(".rumValSelected").dataset.sortvalue);
+        rumSort(document.querySelector(".rumValSelected").dataset.sortvalue);
+        document.querySelector("#rumTracker").classList.add("chosenMixer");
+        let mixerDrinks = document.querySelectorAll("[data-mixers*='[" + mixer + "]'");
+        for (let i = 0; i < mixerDrinks.length; i++) {
+            mixerDrinks[i].classList.add("chosenMixer");
+        }
+        document.querySelector("[data-mixer='" + mixer +"'").classList.add("chosenMixer");
+    }
+    
+}
+
+function UrlExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
+}
+
+
+function rumSortMix(mix) {
+    let mixSortArray = [];
+    for (let i = 0; i < rumListGood.length; i++) {
+        if (rumListGood[i]["mix"][mix]) {
+            let newMix = {};
+            newMix.mix = rumListGood[i]["mix"][mix];
+            newMix.index = rumListGood[i]["index"];
+            mixSortArray.push(newMix);
+        }
+    }    
+
+    rumListSort = JSON.parse(JSON.stringify(mixSortArray));
+
+    rumListSort.sort((b, a) => a["mix"].localeCompare(b["mix"]));
+
+    let allRumElements = document.querySelectorAll(".rum");
+    for (let i = 0; i < allRumElements.length; i++) {
+        allRumElements[i].style.order = "";
+    }
+    for (let i = 0; i < rumListSort.length; i++) {
+        document.querySelector("[data-index='" + rumListSort[i]["index"] + "']").style.order = i;
+    }
+}
---
+++ rum-tracker/index.md
@@ -0,0 +1,63 @@
+---
+layout: layouts/page.njk
+title: Spiced Rum Tracker
+script:
+  - /js/rumTracker/rumList.js
+  - /js/rumTracker/rumTracker.js
+style:
+  - /css/rumTracker.min.css
+eleventyNavigation:
+  key: Games
+  order: 1
+---
+
+Welcome to the Dood Vei Spiced Rum Tracker!
+
+Here you can see all of the spiced rums I've drunk. Sort by a wide variety of traits, or filter by your favourite consumption vector.
+
+**Note on ratings:** I can honestly say all of these rums were at least ok. Outside of one in particular I enjoyed every single drink of these I had, regarldess of what I mixed them with. Neat is a different story, but that's fine. I like to like things, especially when those things are spiced rums.
+
+<h2>A whole bunch o rums</h2>
+
+<ul id="rumSorter">
+</ul>
+
+<ul id="rumMixerPicker">
+</ul>
+
+<div id="rumTracker">
+  <div>
+  <div id="rumBig">
+    <div class="rum">
+      <div class="rumInfo"></div>
+      <div class="rumImage">
+        <img src="rums/ohara.png" >
+      </div>  
+    </div>
+  </div>
+  </div>
+
+  <div id="rumSingle">
+    <div id="rumSticky">
+      <div id="rumClose">x</div>
+      <div id="rumSingleText">
+        <h2 id="rumSingleTitle">Kraken</h2>
+        <div id="rumStats">
+          <div>
+            <p>Price: £<span id="rumPrice">17.93</span><br>
+            Volume: <span id="rumVolume">100</span>ml<br>
+            Percentage: <span id="rumPercent">100</span>%<br>
+            Total units: <span id="rumUnits">51</span><br>
+            Price per unit: £<span id="rumUnitPrice">3.01</span></p>
+            <h3>Style</h3>
+            <div id="rumStyleBox"></div>
+          </div>
+          <div id="rumStatsImg"><img src="rums/kraken.png"></div>
+        </div>
+        <div id="rumTasty"><h3>Quality of Tastings</h3></div>
+        <h3>Notes</h3>
+        <p id="rumNotes">Super tasty or whatever.</p>
+      </div>
+    </div>
+  </div>
+</div>
