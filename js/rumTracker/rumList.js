6db92b1bcc435a32bd382a83a5b121ea903e81c2
SuperDrDood <super.dr.dood@gmail.com>
2022-07-23 12:17:38 +0100

    Rum tracker update

--- js/rumTracker/rumList.js
+++ js/rumTracker/rumList.js
@@ -1,37 +1,40 @@
-rumList2 = `Name|Price|Volume|Percentage|Units|Price per Unit|Height|Style|Notes|MIX|Neat|Dr Pepper|Ginger Beer|Pineapple Juice
-Kraken|20.50|700|40|28|0.73|245|4|I love this stuff. Almost definitely the spirit I’ve drunk most of in my life.|MIX|4|5|4|4
-Peaky Blinder|19.90|700|40|28|0.71|270|3|A tie-in with the show Peaky Blinders. I’ve never seen the show, but the rum is pretty nice.|MIX|2|4|3|
-Old J|18.00|700|35|24.5|0.73|290|2|Cheap and acceptable.|MIX|2|4|3|
-Black Magic|19.49|700|40|28|0.70|245|3|Cool name, less cool rum.|MIX|2|3|3|
-Cockspur Balla Black|23.94|700|40|28|0.86|220|5|Awesome bottle design! Tastes like coffee which I’m not a fan of.|MIX|1|3|2|
-Five Hundred Cuts|23.95|700|40|28|0.86|224|4|A very herbal rum. Nice and unique, for those with a more refined taste and have always wanted to drink pot pourri.|MIX|3|4|2|
-Bush Rum|18.00|700|37.5|26.25|0.69|183|2|The bottle and packaging are made from 100% recycled materials. A worthy goal for an inexpensive rum.|MIX|2|3|3|
-Cut to the Spice|23.79|700|37.5|26.25|0.91|246|3|Clearly designed by a designer, if that makes sense. A nice rum.|MIX|2|4|4|
-Rebellion|19.75|700|37.5|26.25|0.75|294|2|Bland.|MIX|2|3|3|
-RedLeg|15.00|700|37.5|26.25|0.57|260|3|Very nice, very cheap.|MIX|2|4|3|
-Lambs|12.50|700|30|21|0.60|284|3|Only 30% alcohol so seems unfair to put this on the list. But I have, and it’s delicious.|MIX|4|4|4|
-Tovess|18.72|700|35|24.5|0.76|218|2|This is Amazon Basics own-brand spiced rum. It is BY FAR the worst spiced rum I’ve ever had. Every other rum I would probably drink again. Not this. Never this.|MIX|1|1|1|
-Dead Man’s Fingers|17.99|700|37.5|26.25|0.69|245|4|Cool name, cool design, very tasty for the price.|MIX|2|4|4|
-Two Swallows|25.00|700|38|26.6|0.94|224|3|Puntastic name as the label has two birds on it.|MIX|2|4|3|
-Depth Charge|23.75|700|40|28|0.85|215|4|Really cool name and labelling, pretty foul tasting|MIX|1|3|2|
-Horizon Black Hole|19.95|700|42|29.4|0.68|255|3|A love space so I was hoping to love this rum. Alas, hopes never survive reality.|MIX|1|3|2|
-8Track|28.50|700|37.5|26.25|1.09|220|4|A slightly more expensive rum, and actually tastes it. Nice and citrusy and a cool design on the bottle.|MIX|3|4|4|
-Dark Matter|26.00|700|40|28|0.93|223|5|Awesome bottle that looks very science-y, makes you think you’re drinking chemicals. Unsurprisingly tastes like chemicals.|MIX|1|3|2|
-Jack Ratt Lyme Bay Lugger|34.90|700|40|28|1.25|250|5|A very expensive rum but has a wonderfully unique bottle. And tastes high quality. Highly recommended, but only for special occassions.|MIX|4|5|4|5
-Rumbullion|27.00|700|42.6|29.82|0.91|224|5|I got this on deal, but still incredibly overpriced. The bottle is really cool as the top is sealed with wax and therefore fun to open but the rum inside is incredibly generic. Has a smoothness that belies it’s high percentage.|MIX|3|3|2|4
-Wild Tiger|21.34|700|38|26.6|0.80|265|5|The bottle is covered in fur! Amazing! An Indian rum that donates to a tiger charity. Quite cheap, and the rum is nice.|MIX|2|4|3|4
-Barti Ddu|25.58|700|35|24.5|1.04|220|3|Has seaweed in apparently, though didn’t taste very seaweedy. Quite nice.|MIX|2|4|3|4
-Duppy Share with Pineapple and Kola Nut|23.94|700|37.5|26.25|0.91|235|2|Has a mild pineapple flavour. I’ve no idea what a kola nut us, and I’m not going to look it up. The only investigation you should do when drinking spiced rum is with your tongue.|MIX|3|4|3|4
-Bayou|21.50|700|40|28|0.77|286|3|Thick bottle. Ok rum.|MIX|2|3|3|3
-Nusa Caña|21.95|700|40|28|0.78|325|4|One of the best bottle designs, but the rum was nothing special.|MIX|2|3|3|4
-Rumshack|24.20|1500|35|52.5|0.46|300|1|The bottle is upside down, I guess because this is made for using in bars. Also it’s double the size of every other bottle which broke this page design so it’s the only bottle here out of scale. Very cheap per unit, and not too terrible. Probably the best choice if you want alcohol cheaply.|MIX|2|3|3|4
-Pirate’s Grog|24.67|700|37.5|26.25|0.94|245|3|Cool name but a generic rum.|MIX|3|3|3|4
-Jah45|20.99|700|40|28|0.75|290|3|Sort of smells like urinal cakes, but in a good way?|MIX|2|3|2|3
-Rum Sixty Six|18.06|700|37.5|26.25|0.69|310|2|An excellent rum that’s also extremely cheap. Brilliant!|MIX|3|5|4|5
-Sailor Jerry’s|14.00|700|40|28|0.50|292|3|A true classic in the spiced rum world. It’s tasty, it’s cheap, it has a hula girl.|MIX|3|4|4|4
-Mooncurser|22.50|700|37.5|26.25|0.86|295|3|Really awesome name that the design doesn’t live up to.|MIX|2|3|3|3
-Siren’s Call|21.00|700|37.5|26.25|0.80|238|3|Nice name and bottle design. A standard spiced rum.|MIX|2|4|3|4
-O'Hara's Spiced Rum|23.95|700|37.5|26.25|0.91|290|3|Tastes very smoky which I am a fan of and has a lovely parrot on the bottle.|MIX|2|4|3|4
-Pink Pigeon Original|24.45|700|40|28|0.87|307|2|This has lots of vanilla which is adorable.|MIX|3|4|3|4
-Bounty Spiced Rum|21.85|700|40|28|0.78|295|2|Slightly below average taste. Nothing more to say.|MIX|1|3|2|3
-Stroh Inländer 40|22.39|700|40|28|0.80|215|3|Tastes like paint thinner. Mixing it with anything doesn’t make that go away.|MIX|1|2|2|2`;
+rumList2 = `Name|Price|Volume|Percentage|Units|Price per Unit|Height|Style|Notes|MIX|Neat|Dr Pepper|Ginger Beer|Pineapple Juice|Coca-Cola
+Kraken|20.50|700|40|28|0.73|245|4|I love this stuff. Almost definitely the spirit I’ve drunk most of in my life.|MIX|4|5|4|4|
+Peaky Blinder|19.90|700|40|28|0.71|270|3|A tie-in with the show Peaky Blinders. I’ve never seen the show, but the rum is pretty nice.|MIX|2|4|3||
+Old J|18.00|700|35|24.5|0.73|290|2|Cheap and acceptable.|MIX|2|4|3||
+Black Magic|19.49|700|40|28|0.70|245|3|Cool name, less cool rum.|MIX|2|3|3||
+Cockspur Balla Black|23.94|700|40|28|0.86|220|5|Awesome bottle design! Tastes like coffee which I’m not a fan of.|MIX|1|3|2||
+Five Hundred Cuts|23.95|700|40|28|0.86|224|4|A very herbal rum. Nice and unique, for those with a more refined taste and have always wanted to drink pot pourri.|MIX|3|4|2||
+Bush Rum|18.00|700|37.5|26.25|0.69|183|2|The bottle and packaging are made from 100% recycled materials. A worthy goal for an inexpensive rum.|MIX|2|3|3||
+Cut to the Spice|23.79|700|37.5|26.25|0.91|246|3|Clearly designed by a designer, if that makes sense. A nice rum.|MIX|2|4|4||
+Rebellion|19.75|700|37.5|26.25|0.75|294|2|Bland.|MIX|2|3|3||
+RedLeg|15.00|700|37.5|26.25|0.57|260|3|Very nice, very cheap.|MIX|2|4|3||
+Lambs|12.50|700|30|21|0.60|284|3|Only 30% alcohol so seems unfair to put this on the list. But I have, and it’s delicious.|MIX|4|4|4||
+Tovess|18.72|700|35|24.5|0.76|218|2|This is Amazon Basics own-brand spiced rum. It is BY FAR the worst spiced rum I’ve ever had. Every other rum I would probably drink again. Not this. Never this.|MIX|1|1|1||
+Dead Man’s Fingers|17.99|700|37.5|26.25|0.69|245|4|Cool name, cool design, very tasty for the price.|MIX|2|4|4||
+Two Swallows|25.00|700|38|26.6|0.94|224|3|Puntastic name as the label has two birds on it.|MIX|2|4|3||
+Depth Charge|23.75|700|40|28|0.85|215|4|Really cool name and labelling, pretty foul tasting|MIX|1|3|2||
+Horizon Black Hole|19.95|700|42|29.4|0.68|255|3|A love space so I was hoping to love this rum. Alas, hopes never survive reality.|MIX|1|3|2||
+8Track|28.50|700|37.5|26.25|1.09|220|4|A slightly more expensive rum, and actually tastes it. Nice and citrusy and a cool design on the bottle.|MIX|3|4|4||
+Dark Matter|26.00|700|40|28|0.93|223|5|Awesome bottle that looks very science-y, makes you think you’re drinking chemicals. Unsurprisingly tastes like chemicals.|MIX|1|3|2||
+Jack Ratt Lyme Bay Lugger|34.90|700|40|28|1.25|250|5|A very expensive rum but has a wonderfully unique bottle. And tastes high quality. Highly recommended, but only for special occassions.|MIX|4|5|4|5|
+Rumbullion|27.00|700|42.6|29.82|0.91|224|5|I got this on deal, but still incredibly overpriced. The bottle is really cool as the top is sealed with wax and therefore fun to open but the rum inside is incredibly generic. Has a smoothness that belies it’s high percentage.|MIX|3|3|2|4|
+Wild Tiger|21.34|700|38|26.6|0.80|265|5|The bottle is covered in fur! Amazing! An Indian rum that donates to a tiger charity. Quite cheap, and the rum is nice.|MIX|2|4|3|4|
+Barti Ddu|25.58|700|35|24.5|1.04|220|3|Has seaweed in apparently, though didn’t taste very seaweedy. Quite nice.|MIX|2|4|3|4|
+Duppy Share with Pineapple and Kola Nut|23.94|700|37.5|26.25|0.91|235|2|Has a mild pineapple flavour. I’ve no idea what a kola nut us, and I’m not going to look it up. The only investigation you should do when drinking spiced rum is with your tongue.|MIX|3|4|3|4|
+Bayou|21.50|700|40|28|0.77|286|3|Thick bottle. Ok rum.|MIX|2|3|3|3|
+Nusa Caña|21.95|700|40|28|0.78|325|4|One of the best bottle designs, but the rum was nothing special.|MIX|2|3|3|4|
+Rumshack|24.20|1500|35|52.5|0.46|300|1|The bottle is upside down, I guess because this is made for using in bars. Also it’s double the size of every other bottle which broke this page design so it’s the only bottle here out of scale. Very cheap per unit, and not too terrible. Probably the best choice if you want alcohol cheaply.|MIX|2|3|3|4|
+Pirate’s Grog|24.67|700|37.5|26.25|0.94|245|3|Cool name but a generic rum.|MIX|3|3|3|4|
+Jah45|20.99|700|40|28|0.75|290|3|Sort of smells like urinal cakes, but in a good way?|MIX|2|3|2|3|
+Rum Sixty Six|18.06|700|37.5|26.25|0.69|310|2|An excellent rum that’s also extremely cheap. Brilliant!|MIX|3|5|4|5|
+Sailor Jerry’s|14.00|700|40|28|0.50|292|3|A true classic in the spiced rum world. It’s tasty, it’s cheap, it has a hula girl.|MIX|3|4|4|4|
+Mooncurser|22.50|700|37.5|26.25|0.86|295|3|Really awesome name that the design doesn’t live up to.|MIX|2|3|3|3|
+Siren’s Call|21.00|700|37.5|26.25|0.80|238|3|Nice name and bottle design. A standard spiced rum.|MIX|2|4|3|4|
+O'Hara's Spiced Rum|23.95|700|37.5|26.25|0.91|290|3|Tastes very smoky which I am a fan of and has a lovely parrot on the bottle.|MIX|2|4|3|4|
+Pink Pigeon Original|24.45|700|40|28|0.87|307|2|This has lots of vanilla which is adorable.|MIX|3|4|3|4|
+Bounty Spiced Rum|21.85|700|40|28|0.78|295|2|Slightly below average taste. Nothing more to say.|MIX|1|3|2|3|
+Stroh Inländer 40|22.39|700|40|28|0.80|215|3|Tastes like paint thinner. Mixing it with anything doesn’t make that go away.|MIX|1|2|2|2|
+Captain Cook’s Spiced Black|7.99|700|40|28|0.29|280|2|Had this in Lanzarote. Has a cool boat on front and SUPER cheap.|MIX|2|||3|3
+Five Hundred Cuts (Re-brand)|25.00|700|40|28|0.89|253|2|This is a re-brand of the other Five Hundred Cuts. I’m sure they’ve redone the recipe too as before it was very herbally, and this tastes like a standard spiced rum. The bottle doesn’t look as cool either. When it rains it pours at BrewDog I suppose. That should be some sort of tagline for them.|MIX|2|4|3|4|
+Hell or High Water|19.99|700|38|26.6|0.75|225|4|Cool name, nice flavour, and a good price as I got it on deal. A lovely, if tad generic spiced rum. A little bit of investigating (reading a note tied to the neck of the bottle) told me this used to be a Ron Jeremy signature rum. What? Apparently they had to re-brand because he’s a sex pest now. Spiced rum drama is spicy!|MIX|2|4|3|4|`;
